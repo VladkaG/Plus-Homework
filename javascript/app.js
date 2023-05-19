57bae010f237be7e75b07ff5737e19f26ff6d883
Vladyslava <fireflyingggg@gmail.com>
2023-05-20 00:25:45 +0300

    Implemented unit conversation

--- javascript/app.js
+++ javascript/app.js
@@ -27,38 +27,23 @@ function showDate(timestamp) {
 
 /*******************************************************************/
 
-let temperatureElement = document.querySelector("#temperature");
-let temperature = temperatureElement.innerHTML;
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", function (event) {
-  event.preventDefault();
-  temperatureElement.innerHTML = Math.round(temperature * 1.8 + 32);
-});
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", function (event) {
-  temperatureElement.innerHTML = temperature;
-});
-
-/*******************************************************************/
-
 let apiKey = "5t4badf2211oab190e2bd035f7fefd1a";
 let form = document.querySelector("#search-form");
 let locationButton = document.querySelector("#location-button");
+let celsiusTemperature = null;
 
 function search(city) {
   let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(url).then(displayWeather);
 }
-debugger;
 function displayWeather(response) {
   let iconUrl = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/`;
+
+  celsiusTemperature = response.data.temperature.current;
   document.querySelector("h1").innerHTML = response.data.city;
 
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.temperature.current
-  );
+  document.querySelector("#temperature").innerHTML =
+    Math.round(celsiusTemperature);
   document.querySelector("#feels-like").innerHTML = Math.round(
     response.data.temperature.feels_like
   );
@@ -107,86 +92,20 @@ locationButton.addEventListener("click", function (event) {
 search("Kharkiv");
 
 /*******************************************************************/
-
-/*let monMax = document.querySelector("#mon-max");
-let monMin = document.querySelector("#mon-min");
-let monIcon = document.querySelector("#mon-icon");
-
-let tueMax = document.querySelector("#tue-max");
-let tueMin = document.querySelector("#tue-min");
-let tueIcon = document.querySelector("#tue-icon");
-
-let wedMax = document.querySelector("#wed-max");
-let wedMin = document.querySelector("#wed-min");
-let wedIcon = document.querySelector("#wed-icon");
-
-let thurMax = document.querySelector("#thur-max");
-let thurMin = document.querySelector("#thur-min");
-let thurIcon = document.querySelector("#thur-icon");
-
-let friMax = document.querySelector("#fri-max");
-let friMin = document.querySelector("#fri-min");
-let friIcon = document.querySelector("#fri-icon");
-
-form.addEventListener("submit", function (event) {
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+let celsiusLink = document.querySelector("#celsius-link");
+fahrenheitLink.addEventListener("click", function (event) {
   event.preventDefault();
-  let url = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput.value}&appid=${apiKey}`;
-  axios.get(url).then(function (response) {
-    let lat = response.data[0].lat;
-    let lon = response.data[0].lon;
-
-    let coordUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-    axios.get(coordUrl).then(function (response) {
-      monMin.innerHTML = Math.round(response.data.list[0].main.temp_min);
-      monMax.innerHTML = Math.round(response.data.list[0].main.temp_max);
-      monIcon.src = `https://openweathermap.org/img/wn/${response.data.list[0].weather[0].icon}@2x.png`;
-
-      tueMin.innerHTML = Math.round(response.data.list[0].main.temp_min);
-      tueMax.innerHTML = Math.round(response.data.list[0].main.temp_max);
-      tueIcon.src = `https://openweathermap.org/img/wn/${response.data.list[0].weather[0].icon}@2x.png`;
-
-      wedMin.innerHTML = Math.round(response.data.list[0].main.temp_min);
-      wedMax.innerHTML = Math.round(response.data.list[0].main.temp_max);
-      wedIcon.src = `https://openweathermap.org/img/wn/${response.data.list[0].weather[0].icon}@2x.png`;
-
-      thurMin.innerHTML = Math.round(
-        Math.min(
-          response.data.list[3].main.temp_min,
-          response.data.list[4].main.temp_min,
-          response.data.list[5].main.temp_min,
-          response.data.list[6].main.temp_min
-        )
-      );
-      thurMax.innerHTML = Math.round(
-        Math.max(
-          response.data.list[3].main.temp_max,
-          response.data.list[4].main.temp_max,
-          response.data.list[5].main.temp_max,
-          response.data.list[6].main.temp_max
-        )
-      );
-      thurIcon.src = `https://openweathermap.org/img/wn/${response.data.list[6].weather[0].icon}@2x.png`;
-
-      friMin.innerHTML = Math.round(
-        Math.min(
-          response.data.list[9].main.temp_min,
-          response.data.list[11].main.temp_min,
-          response.data.list[13].main.temp_min,
-          response.data.list[16].main.temp_min
-        )
-      );
-      friMax.innerHTML = Math.round(
-        Math.max(
-          response.data.list[9].main.temp_max,
-          response.data.list[11].main.temp_max,
-          response.data.list[13].main.temp_max,
-          response.data.list[16].main.temp_max
-        )
-      );
-      friIcon.src = `https://openweathermap.org/img/wn/${response.data.list[14].weather[0].icon}@2x.png`;
-
-      console.log(response.data);
-    });
-  });
+  document.querySelector("#temperature").innerHTML = Math.round(
+    (celsiusTemperature * 9) / 5 + 32
+  );
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
 });
-*/
+celsiusLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  document.querySelector("#temperature").innerHTML =
+    Math.round(celsiusTemperature);
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+});
